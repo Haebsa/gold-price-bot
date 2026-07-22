97895ff16cc010a36e769b1298a836b5d5f20283
Haebsa <hamidebisoft@gmail.com>
2026-07-22 22:42:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Gold Price Bot Started")
+import os
+import requests
+
+TOKEN = os.environ.get("BALE_TOKEN")
+
+CHANNEL_ID = "اینجا شناسه کانال بله را بگذار"
+
+message = """
+✅ ربات قیمت طلا و دلار فعال شد
+
+سیستم ارسال خودکار با موفقیت راه‌اندازی شد.
+"""
+
+url = f"https://tapi.bale.ai/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHANNEL_ID,
+    "text": message
+}
+
+response = requests.post(url, data=data)
+
+print(response.text)
