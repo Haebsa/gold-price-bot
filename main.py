f8fab374eb7a3f5f367fbcf2d7742e678c8e39f0
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:08:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
 
-for cid in [28069, 28070, 28071, 28072, 28073]:
-    print("=" * 50)
-    print("Category:", cid)
-
-    url = f"https://api.tgju.org/v1/market/list-data?category_ids={cid}&extra_data=1&lang=fa"
-
+for cid in range(28050, 28090):
     try:
-        data = requests.get(url).json()["data"]
+        url = f"https://api.tgju.org/v1/market/list-data?category_ids={cid}&extra_data=1&lang=fa"
+        data = requests.get(url, timeout=5).json().get("data", [])
 
-        for row in data[:5]:
-            print(row[0])
-
-    except Exception as e:
-        print(e)
+        if data:
+            print(f"\n===== Category {cid} =====")
+            for row in data[:3]:
+                print(row[0])
+    except:
+        pass
