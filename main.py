b42120d19b0f44b9d18dbc6f4b562685c60275bd
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:06:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import requests
 
-url = "https://api.tgju.org/v1/market/list-data?category_ids=28070&extra_data=1&lang=fa"
+for cid in [28069, 28070, 28071, 28072, 28073]:
+    print("=" * 50)
+    print("Category:", cid)
 
-data = requests.get(url).json()["data"]
+    url = f"https://api.tgju.org/v1/market/list-data?category_ids={cid}&extra_data=1&lang=fa"
 
-for row in data:
-    if "geram18" in row[0] or "price_dollar_rl" in row[0]:
-        print(row)
+    try:
+        data = requests.get(url).json()["data"]
+
+        for row in data[:5]:
+            print(row[0])
+
+    except Exception as e:
+        print(e)
