1161c0ee4759ba025bc0c2003b9b5ae08c7f7777
Haebsa <hamidebisoft@gmail.com>
2026-07-23 12:17:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 import os
-import requests
 
-TOKEN = os.environ["BALE_TOKEN"]
-
-url = f"https://tapi.bale.ai/bot{TOKEN}/getUpdates"
-
-response = requests.get(url)
-
-print(response.status_code)
-print(response.text)
+print(os.environ)
