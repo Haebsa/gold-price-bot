d8417b7453c667c870b71bce7eb8ced53debe1bf
Haebsa <hamidebisoft@gmail.com>
2026-07-23 14:43:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,76 +2,111 @@ import os
 import re
 import requests
 
+# =====================
+# توکن‌ها
+# =====================
 
-TOKEN = os.environ["BALE_TOKEN"]
+BALE_TOKEN = os.environ["BALE_TOKEN"]
+RUBIK_TOKEN = os.environ["RUBIK_TOKEN"]
 
-CHANNEL_ID = "@goldha"
 
-# دریافت قیمت دلار از API رسمی TGJU
+# =====================
+# کانال‌ها
+# =====================
+
+BALE_CHANNEL = "@goldha"
+RUBIKA_CHANNEL = "@gold_pric"
+
+
+# =====================
+# دریافت قیمت دلار TGJU
+# =====================
+
 url = "https://api.tgju.org/v1/market/list-data?category_ids=28070&extra_data=1&lang=fa"
 
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-data = requests.get(url, headers=headers).json()["data"]
+response = requests.get(url, headers=headers)
+
+data = response.json()["data"]
+
 
 price = None
 time = None
 
+
 for row in data:
-    if 'profile/price_dollar_rl' in row[0]:
+    if "profile/price_dollar_rl" in row[0]:
         price = re.sub("<.*?>", "", row[1]).strip()
         time = row[2]
         break
 
-message = f"""💵 قیمت لحظه‌ای دلار
+
+if price is None:
+    print("قیمت پیدا نشد")
+    exit()
+
+
+# =====================
+# متن پیام
+# =====================
+
+message = f"""
+💵 قیمت لحظه‌ای دلار
+
 💰 {price} ریال
+
 🕒 آخرین بروزرسانی:
 {time}
 
-📢 @goldha"""
-
-send_url = f"https://tapi.bale.ai/bot{TOKEN}/sendMessage"
-
-response = requests.post(send_url, data={
-    "chat_id": CHANNEL_ID,
-    "text": message
-})
-
-
-
-
-print(response.status_code)
-print(response.text)
-
-import requests
-
-TOKEN2 = "CABFGI0XPZOLZGBRNVUBSVWJERIIJXRMCOITJBAENARPAUYCGVHBEFJHDXOHEETC"
-CHANNEL2 = "@gold_pric"
-
-text = """
-💰 قیمت لحظه‌ای
-
-💵 دلار:
-1,922,000 تومان
-
-🕒 بروزرسانی خودکار
+📢 @goldha
 """
 
-url = f"https://botapi.rubika.ir/v3/{TOKEN2}/sendMessage"
 
-data = {
-    "chat_id": CHANNEL2,
-    "text": text
+# =====================
+# ارسال به بله
+# =====================
+
+bale_url = f"https://tapi.bale.ai/bot{BALE_TOKEN}/sendMessage"
+
+
+bale_data = {
+    "chat_id": BALE_CHANNEL,
+    "text": message
 }
 
-r = requests.post(url, json=data)
 
-print(r.text)
-
-
-
-
+bale_response = requests.post(
+    bale_url,
+    data=bale_data
+)
 
 
+print("Bale:")
+print(bale_response.text)
+
+
+
+# =====================
+# ارسال به روبیکا
+# =====================
+
+rubika_url = f"https://botapi.rubika.ir/v3/{RUBIK_TOKEN}/sendMessage"
+
+
+rubika_data = {
+    "chat_id": RUBIKA_CHANNEL,
+    "text": message
+}
+
+
+rubika_response = requests.post(
+    rubika_url,
+    json=rubika_data
+)
+
+
+print("Rubika:")
+print(rubika_response.text)
