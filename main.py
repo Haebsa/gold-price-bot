4ca8f2cb2d43f7563ada994fb1915797c3dff5ff
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:05:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import requests
-import json
 
 url = "https://api.tgju.org/v1/market/list-data?category_ids=28070&extra_data=1&lang=fa"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+data = requests.get(url).json()["data"]
 
-r = requests.get(url, headers=headers)
-
-print(r.status_code)
-print(json.dumps(r.json(), indent=2, ensure_ascii=False)[:6000])
+for row in data:
+    if "geram18" in row[0] or "price_dollar_rl" in row[0]:
+        print(row)
