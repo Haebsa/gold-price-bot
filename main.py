5157b4c5155f5beb94b128551187d92b8b8f7f60
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:17:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
+import os
+import re
 import requests
 
-for cid in range(28050, 28090):
-    try:
-        url = f"https://api.tgju.org/v1/market/list-data?category_ids={cid}&extra_data=1&lang=fa"
-        data = requests.get(url, timeout=5).json().get("data", [])
+TOKEN = os.environ["BALE_TOKEN"]
 
-        if data:
-            print(f"\n===== Category {cid} =====")
-            for row in data[:3]:
-                print(row[0])
-    except:
-        pass
+CHANNEL_ID = "goldha"
+
+# دریافت قیمت دلار از API رسمی TGJU
+url = "https://api.tgju.org/v1/market/list-data?category_ids=28070&extra_data=1&lang=fa"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+data = requests.get(url, headers=headers).json()["data"]
+
+price = None
+time = None
+
+for row in data:
+    if 'profile/price_dollar_rl' in row[0]:
+        price = re.sub("<.*?>", "", row[1]).strip()
+        time = row[2]
+        break
+
+message = f"""💵 قیمت لحظه‌ای دلار
+
+💰 {price} ریال
+
+🕒 آخرین بروزرسانی:
+{time}
+
+📢 @goldha"""
+
+send_url = f"https://tapi.bale.ai/bot{TOKEN}/sendMessage"
+
+response = requests.post(send_url, data={
+    "chat_id": CHANNEL_ID,
+    "text": message
+})
+
+print(response.status_code)
+print(response.text)
