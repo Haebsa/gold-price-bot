6a4b5cff081c029f421837c352c42616b1dddaa8
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:01:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import requests
 import re
 
-url = "https://www.tgju.org/profile/price_dollar_rl"
+html = requests.get(
+    "https://www.tgju.org/profile/price_dollar_rl",
+    headers={"User-Agent": "Mozilla/5.0"}
+).text
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-html = requests.get(url, headers=headers).text
-
-for x in [
-    "api.tgju.org",
-    "__NEXT_DATA__",
-    "price",
-    "fetch(",
-    "axios",
-    "market"
-]:
-    print("======", x)
-    print(x in html)
+for url in re.findall(r'https://[^"\']+', html):
+    if "api.tgju.org" in url:
+        print(url)
