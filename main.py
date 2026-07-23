f9a220913b936aa8ec9eb70bd6e3c2b31bc364f3
Haebsa <hamidebisoft@gmail.com>
2026-07-23 13:02:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
-import re
+import json
 
-html = requests.get(
-    "https://www.tgju.org/profile/price_dollar_rl",
-    headers={"User-Agent": "Mozilla/5.0"}
-).text
+url = "https://api.tgju.org/v1/market/list-data?category_ids=28070&extra_data=1&lang=fa"
 
-for url in re.findall(r'https://[^"\']+', html):
-    if "api.tgju.org" in url:
-        print(url)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+r = requests.get(url, headers=headers)
+
+print(r.status_code)
+print(json.dumps(r.json(), indent=2, ensure_ascii=False)[:6000])
